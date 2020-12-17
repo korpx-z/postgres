1d5ea246fa5ff155d40b1e3b67e607a8dfe1b147
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-12-17 12:38:49 -0500

    Create test.sh

---
+++ 13/alpine/test.sh
@@ -0,0 +1,14 @@
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** testing docker run - postgres *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW Test docker run of Postgres: $ANSI_RESET"
+docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -p 5009:5432 -d quay.io/ibm/postgres:13
+docker inspect some-postgres | grep 'Running'
+
+
+echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
